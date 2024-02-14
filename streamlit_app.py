b190ad40f8c23098870231f27cdeb112ae2880d5
AerialAriana <160053840+AerialAriana@users.noreply.github.com>
2024-02-14 19:07:50 +0100

    Update streamlit_app.py
    changed title (waiting for hint)
    added another header

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,8 +1,10 @@
 import streamlit
-streamlit.title('My Mom's New Healthy Diner')
+streamlit.title('My Moms New Healthy Diner')
 
 streamlit.header('Breakfast Favorits')
 streamlit.text('Omega 3 & Blueberry Oatmeal')
 streamlit.text('Kale, Spinach & Rocket Smoothie')
 streamlit.text('Hard-Boiled Free-Range Egg')
 streamlit.text('Avocado Toast')
+
+streamlit.header('Build Your Own Fruit Smoothie')
